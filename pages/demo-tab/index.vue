cc232e5471391c9026df1f5c11cf1031cb439181
bo.yang <bo.yang@ttpai.cn>
2019-11-05 20:04:28 +0800

    删除无用文件

--- pages/demo-tab/index.vue
+++ pages/demo-tab/index.vue
@@ -19,7 +19,6 @@
 		},
 		data() {
 			return {
-				title: 'Hello'
 			}
 		},
 		onLoad() {
@@ -34,29 +33,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50upx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
 </style>
